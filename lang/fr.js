8b5d1998f60a13b8e2ecbdeb6c6b4bb5b6947c0f
tekkeitserktok <tekkeitserktok@gmail.com>
2017-08-05 15:29:34 +0200

    --

---
+++ lang/fr.js
@@ -0,0 +1,13 @@
+L10N = {
+	"gallery": "Gallerie",
+	"startScreen": "<h1>Photobooth<\/h1>",
+	"takePhoto": "Prendre une photo !",
+	"home": "Accueil",
+	"qr": "QR Code",
+	"newPhoto": "Prendre une photo",
+	"busy": "Ça mouline, ça mouline...",
+	"cheese": "CHEEEEEEEEEEEEEESE !",
+	"qrHelp": "Um das Bild auf dein Handy zu laden, verbinde dich mit dem WLAN <strong>photobooth<\/strong>",
+	"error": "Quelque chose a dérapé ! Vite, allez voir Benjamin !",
+	"reload": "Recharger la page"
+}
